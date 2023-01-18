31b1c8401e2e13c3fefcae63d2bafcfc936390a2
aotlacan <121581398+aotlacan@users.noreply.github.com>
2023-01-18 17:20:03 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,12 +1,12 @@
 var config_data = `
 {
-	"enable_google_sheets": "true",
-  "title": "Scouting PASS 2023",
+  "enable_google_sheets": "true",
+  "title": "818 Scouting",
   "page_title": "Charged Up",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
-	  "gsCol": "scouter",
+      "gsCol": "scouter",
       "code": "s",
       "type": "scouter",
       "size": 5,
@@ -14,7 +14,6 @@ var config_data = `
       "required": "true"
     },
     { "name": "Event",
-	"gsCol": "event",
       "code": "e",
       "type": "event",
       "defaultValue": "2022miroc",
@@ -22,7 +21,6 @@ var config_data = `
       "disabled": "true"
     },
     { "name": "Match Level",
-	"gsCol": "matchLevel",
       "code": "l",
       "type": "level",
       "choices": {
@@ -31,10 +29,9 @@ var config_data = `
         "f": "Finals"
       },
       "defaultValue": "qm",
-      "required": "true"
+      "required": "false"
     },
     { "name": "Match #",
-	"gsCol": "matchNum",
       "code": "m",
       "type": "match",
       "min": 1,
@@ -42,7 +39,6 @@ var config_data = `
       "required": "true"
     },
     { "name": "Robot",
-	"gsCol": "robot",
       "code": "r",
       "type": "robot",
       "choices": {
@@ -53,17 +49,15 @@ var config_data = `
         "r3": "Red-3",
         "b3": "Blue-3"
       },
-      "required":"true"
+      "required":"false"
     },
     { "name": "Team #",
-	"gsCol": "teamNum",
       "code": "t",
       "type": "team",
       "min": 1,
       "max": 99999
     },
     { "name": "Auto Start Position",
-	"gsCol": "startPos",
       "code": "as",
       "type": "field_image",
       "filename": "2023/field_image.png"
@@ -71,42 +65,34 @@ var config_data = `
   ],
   "auton": [
     { "name": "Mobility?",
-	"gsCol": "mobility",
       "code": "am",
       "type": "bool"
     },
     { "name": "High Cube Scored",
-	"gsCol": "HCS",
       "code": "auh",
       "type": "counter"
     },
     { "name": "Medium Cube Scored",
-	"gsCol": "MCS",
       "code": "aum",
       "type": "counter"
     },
     { "name": "Low Cube Scored",
-	"gsCol": "LCS",
       "code": "aul",
       "type": "counter"
     },
     { "name": "High Cone Scored",
-	"gsCol": "HCNS",
       "code": "aoh",
       "type": "counter"
     },
     { "name": "Medium Cone Scored",
-	"gsCol": "MCNS",
       "code": "aom",
       "type": "counter"
     },
     { "name": "Low Cone Scored",
-	"gsCol": "LCNS",
       "code": "aol",
       "type": "counter"
     },
     { "name": "Docked",
-	"gsCol": "dock",
       "code": "ad",
       "type":"radio",
       "choices": {
@@ -120,57 +106,46 @@ var config_data = `
   ],
   "teleop": [
     { "name": "Cycle Timer",
-	"gsCol": "cycleTimer",
       "code": "tct",
       "type": "cycle"
     },
     { "name": "High Cube Scored",
-	"gsCol": "HCST",
       "code": "tuh",
       "type": "counter"
     },
     { "name": "Medium Cube Scored",
-	"gsCol": "MCST",
       "code": "tum",
       "type": "counter"
     },
     { "name": "Low Cube Scored",
-	"gsCol": "LCST",
       "code": "tul",
       "type": "counter"
     },
     { "name": "High Cone Scored",
-	"gsCol": "HCNST",
       "code": "toh",
       "type": "counter"
     },
     { "name": "Medium Cone Scored",
-	"gsCol": "MCNST",
       "code": "tom",
       "type": "counter"
     },
     { "name": "Low Cone Scored",
-	"gsCol": "LCNST",
       "code": "tol",
       "type": "counter"
     },
     { "name": "Feeder Count",
-	"gsCol": "feederCount",
       "code": "tfc",
       "type": "counter"
     },
     { "name": "Was Defended",
-	"gsCol": "wasDefended",
       "code": "wd",
       "type": "bool"
     },
     { "name": "Smart Placement (creates Links)",
-	"gsCol": "smartPlacement",
       "code": "lnk",
       "type": "bool"
     },
     { "name": "Cargo Intake From",
-	"gsCol": "cargoIntake",
       "code": "cif",
       "type": "radio",
       "choices": {
@@ -184,12 +159,10 @@ var config_data = `
   ],
   "endgame": [
     { "name": "Docking Timer",
-	"gsCol": "dockingTimer",
       "code": "dt",
       "type": "timer"
     },
     { "name": "Final Status",
-	"gsCol": "finalStatus",
       "code": "fs",
       "type":"radio",
       "choices": {
@@ -202,14 +175,12 @@ var config_data = `
       "defaultValue": "x"
     },
     { "name": "# of alliance bots docked/engaged",
-	"gsCol": "botsEngaged",
       "code": "cn",
       "type": "counter"
     }
   ],
   "postmatch": [
     { "name": "Driver Skill",
-	"gsCol": "driverSkill",
       "code": "ds",
       "type": "radio",
       "choices": {
@@ -221,12 +192,10 @@ var config_data = `
       "defaultValue": "x"
     },
     { "name": "Links Scored",
-	"gsCol": "linkScored",
       "code": "ls",
       "type": "counter"
     },
     { "name": "Defense Rating",
-	"gsCol": "defenseRating",
       "code": "dr",
       "type": "radio",
       "choices": {
@@ -239,12 +208,10 @@ var config_data = `
       "defaultValue": "x"
     },
     { "name": "Swerve drive?",
-	"gsCol": "swerveDrive",
       "code": "sd",
       "type": "bool"
     },
     { "name": "Speed Rating",
-	"gsCol": "speedRating",
       "code": "sr",
       "type": "radio",
       "choices": {
@@ -257,30 +224,25 @@ var config_data = `
       "defaultValue":"3"
     },
     { "name": "Died/Tipped",
-	"gsCol": "tippedBot",
       "code": "d",
       "type": "bool"
     },
     { "name": "Avoided coopertition",
-	"gsCol": "noCoopertition",
       "code": "coo",
       "type": "bool"
     },
     { "name": "Make good alliance partner?",
-	"gsCol": "goodPartner",
       "tooltip": "Would you want this robot on your alliance in eliminations?",
       "code": "all",
       "type": "bool"
     },
     { "name": "Comments",
-	"gsCol": "comments",
       "code": "co",
       "type": "text",
       "size": 15,
       "maxSize": 50
     },
     { "name": "Confidence Rating",
-	"gsCol": "confidenceRating",
       "code": "cnf",
       "type": "radio",
       "choices": {
