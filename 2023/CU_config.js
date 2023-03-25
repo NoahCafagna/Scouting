b183ae15d0d841cf522ea16088355aab35fb10f9
NoahCafagna <60243834+NoahCafagna@users.noreply.github.com>
2023-03-25 09:11:15 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mm",
+      "defaultValue": "2023mitry",
       "required": "true",
       "disabled": "true"
     },
