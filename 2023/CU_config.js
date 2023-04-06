f0c34494441eab5a566c6610bac9627437cf7c70
NoahCafagna <60243834+NoahCafagna@users.noreply.github.com>
2023-04-06 08:07:33 -0400

    added micmp4

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mimcc",
+      "defaultValue": "2023micmp4",
       "required": "true",
       "disabled": "true"
     },
