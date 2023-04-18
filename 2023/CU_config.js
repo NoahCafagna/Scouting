f3a7e4b8196f1de71bac3c75417ccaa24b9ae4f9
NoahCafagna <60243834+NoahCafagna@users.noreply.github.com>
2023-04-18 08:05:55 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -2,7 +2,6 @@ var config_data = `
 {
   "title": "Scouting PASS 2023",
   "page_title": "Team 818 Scouting Pass",
-  "team_logo": "2023/Solid Armadillo Logo.png", 
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
