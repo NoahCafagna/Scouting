532a1120445cafe9f54fe3c2f09a956253548437
NoahCafagna <60243834+NoahCafagna@users.noreply.github.com>
2023-04-18 07:43:53 -0400

    changed title

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "Scouting PASS 2023",
-  "page_title": "Charged Up",
+  "page_title": "Team 818 Scouting Pass",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
